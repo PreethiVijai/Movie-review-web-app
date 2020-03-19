a724d584e5f84fb90492aa248b0cb8aa8b3adca0
Tanmai Gajula <usr666@users.noreply.github.com>
2020-03-19 01:47:35 -0600

    Delete producer.py

--- producer.py
+++
@@ -1,5 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='172.40.0.6:9092')
-producer.send('sample', b'Hello, World!')
-producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
-producer.flush()

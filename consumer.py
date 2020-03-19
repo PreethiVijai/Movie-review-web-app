f8b23b97f73fc5c845f3fd837fd12f4c9c79f2d7
Tanmai Gajula <usr666@users.noreply.github.com>
2020-03-19 01:47:10 -0600

    Delete consumer.py

--- consumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-#consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-#consumer.subscribe(['sample'])
-consumer = KafkaConsumer('sample')
-for message in consumer:
-    print(message)

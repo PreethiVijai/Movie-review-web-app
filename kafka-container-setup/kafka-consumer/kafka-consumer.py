19c035020a9bb7aa8d48ede3f0cf21d69d3d227f
srihaasapidikiti <srihaasa.pidikiti@colorado.edu>
2020-03-22 18:09:59 -0600

    not used files deletion

--- kafka-container-setup/kafka-consumer/kafka-consumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer(bootstrap_servers='ims-kafka:9092')
-consumer.subscribe(['sample'])
-#consumer = KafkaConsumer('sample')
-for message in consumer:
-    print(message)

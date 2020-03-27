42800fc2e7df8f2682801f1a5a02c94133520393
srihaasapidikiti <srihaasa.pidikiti@colorado.edu>
2020-03-26 23:56:46 -0600

    mongo-kafka

--- kafka-pipeline-haasa/consumer/consumer-launch.sh
+++ kafka-pipeline-haasa/consumer/consumer-launch.sh
@@ -2,8 +2,8 @@
 #
 # This is the script you need to provide to launch a consumer service
 #
-docker build -t gcr.io/green-entity-251200/consumer:v1 .
-docker push gcr.io/green-entity-251200/consumer:v1
+docker build -t gcr.io/golden-hook-269722/consumer:v1 .
+docker push gcr.io/golden-hook-269722/consumer:v1
 #docker run --rm --network network1 -p  7070:7070 gcr.io/green-entity-251200/rest:v1
-kubectl create deployment consumer --image=gcr.io/green-entity-251200/consumer:v1
+kubectl create deployment consumer --image=gcr.io/golden-hook-269722/consumer:v1
 kubectl expose deployment consumer --port 80 --target-port 80
--- kafka-pipeline-haasa/producer/producer-launch.sh
+++ kafka-pipeline-haasa/producer/producer-launch.sh
@@ -2,8 +2,8 @@
 #
 # This is the script you need to provide to launch a producer service
 #
-docker build -t gcr.io/green-entity-251200/producer:v1 .
-docker push gcr.io/green-entity-251200/producer:v1
+docker build -t gcr.io/golden-hook-269722/producer:v1 .
+docker push gcr.io/golden-hook-269722/producer:v1
 #docker run --rm --network network1 -p  7070:7070 gcr.io/green-entity-251200/rest:v1
-kubectl create deployment producer --image=gcr.io/green-entity-251200/producer:v1
+kubectl create deployment producer --image=gcr.io/golden-hook-269722/producer:v1
 kubectl expose deployment producer --port 80 --target-port 80

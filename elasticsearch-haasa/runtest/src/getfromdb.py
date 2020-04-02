607b0939ab0dae8d11e16938eaafee3073302e08
srihaasapidikiti <srihaasa.pidikiti@colorado.edu>
2020-04-02 12:42:56 -0600

    elastic commit

--- elasticsearch-haasa/runtest/src/getfromdb.py
+++ elasticsearch-haasa/runtest/src/getfromdb.py
@@ -7,11 +7,51 @@ import csv
 import re
 
 username = "scrumlords"
-password = ""
+password = "Bda2020$!"
 
 client = MongoClient("mongodb+srv://scrumlords:"+password+"@cluster0-4ef7e.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client["scraping"]
 tweetCol = db["movies_test"]    
+db1 = client["realTime"]
+tweetCol1 = db1["tweets_test_1"]
 for x in tweetCol.find({},{ "_id": 0 }):
-  print("new line")  
-  print(x)
+    #print(x)
+    #print("new line")  
+    #print(type(x[u'movieId']))
+    movieId=x[u'movieId'].encode('ascii', 'replace')
+    #print(type(movieId))
+    myquery = {"movieId":movieId}
+    count=0
+    avgsentiment=0
+    tweetLocations= []
+    for doc in tweetCol1.find(myquery):
+        #print("entered")
+        count+=1
+        tweetLocations.append(doc[u'user_place'])
+        avgsentiment+=doc[u'sentimentScore']
+    if count!=0:    
+        avgsentiment= avgsentiment/count
+        print(avgsentiment) 
+    else:
+        avgsentiment= None       
+    x1 = {'name': x[u'name'],
+    'movieId': x[u'movieId'],
+    'year': x[u'year'],
+    'runtime': x[u'runtime'],
+    'language': x[u'language'],
+    'cast': x[u'cast'],
+    'imageurl': x[u'imageurl'],
+    'trailer': x[u'trailer'],
+    'plot': x[u'plot'],
+    'rating': x[u'rating'],
+    'genreList': x[u'genreList'],
+    'reviewsList': x[u'reviewsList'],
+    'watchList': x[u'watchList'],
+    'avgsentiment':avgsentiment,
+    'tweetLocations': tweetLocations
+    }
+
+    #res = es.index(index="test-index", doc_type='tweet', id=x[u'movieId'], body=x1)
+    #print("x1")
+    #print(x1)
+    #print("new")
--- elasticsearch-haasa/runtest/src/test.py
+++ elasticsearch-haasa/runtest/src/test.py
@@ -10,25 +10,56 @@ password = ""
 client = MongoClient("mongodb+srv://scrumlords:"+password+"@cluster0-4ef7e.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client["scraping"]
 tweetCol = db["movies_test"]    
+db1 = client["realTime"]
+tweetCol1 = db1["tweets_test_1"]
 for x in tweetCol.find({},{ "_id": 0 }):
-    print(x)
-    print("new line")  
-    print(x[u'movieId'])
-  
-    #doc = 
-    #{'author': 'kimchy',
-    #'text': 'Elasticsearch: cool. bonsai cool.',
-    #'timestamp': datetime.now(),
-    #}
-
-    res = es.index(index="test-index", doc_type='tweet', id=x[u'movieId'], body=x)
-    print(res)
+    #print(x)
+    #print("new line")  
+    #print(type(x[u'movieId']))
+    movieId=x[u'movieId'].encode('ascii', 'replace')    
+    movieId= movieId.decode("utf-8")
+    #print(type(movieId))
+    myquery = {"movieId":movieId}
+    count=0
+    avgsentiment=0
+    tweetLocations= []
+    for doc in tweetCol1.find(myquery):
+        #print("entered")
+        count+=1
+        tweetLocations.append(doc[u'user_place'])
+        avgsentiment+=doc[u'sentimentScore']
+    if count!=0:    
+        avgsentiment= avgsentiment/count
+        #print(avgsentiment) 
+    else:
+        avgsentiment= None       
+    x1 = {'name': x[u'name'],
+    'movieId': x[u'movieId'],
+    'year': x[u'year'],
+    'runtime': x[u'runtime'],
+    'language': x[u'language'],
+    'cast': x[u'cast'],
+    'imageurl': x[u'imageurl'],
+    'trailer': x[u'trailer'],
+    'plot': x[u'plot'],
+    'rating': x[u'rating'],
+    'genreList': x[u'genreList'],
+    'reviewsList': x[u'reviewsList'],
+    'watchList': x[u'watchList'],
+    'avgsentiment':avgsentiment,
+    'tweetLocations': tweetLocations
+    }
+    #print(x1)
+    res = es.index(index="test-index", doc_type='tweet', id=movieId, body=x1)
+    #print(res)
 
 #res = es.get(index="test-index", doc_type='tweet', id=1)
 #print(res)
 
 es.indices.refresh(index="test-index")
 res = es.search(index="test-index", body={"query": {"match_all": {}}})
+#res=es.get(index='test-index',doc_type='tweet',id="tt1051906")
+#print(res)
 print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
     print("%(name)s %(year)s" % hit["_source"])

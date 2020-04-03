84380bd500b61078a8feb37aa0a9795f4f1e5505
Shruthi1111 <shruthishridharan95@gmail.com>
2020-04-02 19:54:18 -0600

    Integrating trailer, where to watch and plot

--- React-UI/src/components/counter.jsx
+++ React-UI/src/components/counter.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./counter.css";
 import logo from "./cine.png";
+import netflix_icon from "./netflix.png";
+import itunes_icon from "./itunes.png";
+import hulu_icon from "./hulu-icon.png";
+import vudu_icon from "./vudu.png";
 import search from "./searchicon.jpg";
 import Autosuggest from "react-autosuggest";
 import axios from "axios";
@@ -78,13 +82,81 @@ class Counter extends Component {
   render() {
     let videoid = "vi2308751129";
     let link = `https://www.imdb.com/videoembed/${videoid}`;
+    let plotval = "";
+    /* Initialize all hrefs */
+    let fandango_href = "http://www.fandangonow.com";
+    let netflix_href = "http://www.netflix.com";
+    let itunes_href = "http://www.itunes.com";
+    let hulu_href = "http://www.hulu.com";
+    let vudu_href = "http://www.vudu.com";
+
+    /*To show disable icons*/
+    let nset = false,
+      iset = false,
+      hset = false,
+      vset = false,
+      fset = false;
     const value = this.state.value;
     const suggestions = this.state.suggestions;
     if (this.state.filterResults != null) {
-      let videoid = this.state.filterResults[0].trailer;
+      var results = this.state.filterResults[0];
+      let videoid = results.trailer;
       let link = `https://www.imdb.com/videoembed/${videoid}`;
       document.getElementById("iframeid").src = link;
+
+      let plotval = results.plot;
+      document.getElementById("plot_Val").textContent = plotval;
+      if (results.watchList != null) {
+        for (var i in results.watchList) {
+          var href = results.watchList[i];
+          if (href.includes("netflix")) {
+            netflix_href = href;
+            nset = true;
+            document.getElementById("netflix_watchlink").href = netflix_href;
+          } else if (href.includes("hulu")) {
+            hulu_href = href;
+            hset = true;
+            document.getElementById("hulu_watchlink").href = hulu_href;
+          } else if (href.includes("vudu")) {
+            vudu_href = href;
+            vset = true;
+            document.getElementById("vudu_watchlink").href = vudu_href;
+          } else if (href.includes("itunes")) {
+            itunes_href = href;
+            iset = true;
+            document.getElementById("itunes_watchlink").href = itunes_href;
+          } else if (href.includes("fandango")) {
+            fandango_href = href;
+            fset = true;
+            document.getElementById("fandango_watchlink").href = fandango_href;
+          }
+        }
+        function changeIcons(watchlink, idName) {
+          document.getElementById(watchlink).href = "#";
+          document.getElementById(watchlink).title =
+            "This movie is not available on netflix";
+          console.log(document.getElementById(idName).style);
+          document.getElementById(idName).style.opacity = "0.3";
+          document.getElementById(idName).style.cursor = "default";
+        }
+        if (nset == false) {
+          changeIcons("netflix_watchlink", "netflix");
+        }
+        if (hset == false) {
+          changeIcons("hulu_watchlink", "hulu");
+        }
+        if (vset == false) {
+          changeIcons("vudu_watchlink", "vudu");
+        }
+        if (fset == false) {
+          changeIcons("fandango_watchlink", "fandango");
+        }
+        if (iset == false) {
+          changeIcons("itunes_watchlink", "itunes");
+        }
+      }
     }
+
     const inputProps = {
       placeholder: " Enter movie name or year",
       value,
@@ -129,36 +201,111 @@ class Counter extends Component {
         </div>
         {/* header div ends here */}
         <div id="all_details">
-          <div id="moviereview">
-            <div id="trailer" style={{ backgroundColor: "#cde" }}>
-              <iframe
-                id="iframeid"
-                src={link}
-                width="550"
-                height="260"
-                allowfullscreen="true"
-                mozallowfullscreen="true"
-                webkitallowfullscreen="true"
-                frameborder="no"
-                scrolling="no"
-              ></iframe>
+          <div id="row1">
+            <iframe
+              id="iframeid"
+              src={link}
+              width="570"
+              height="280"
+              allowfullscreen="true"
+              mozallowfullscreen="true"
+              webkitallowfullscreen="true"
+              frameborder="no"
+              scrolling="no"
+            ></iframe>
+            <div id="where_to_watch">
+              <div id="netflix_hulu_vudu">
+                <div id="netflix_div">
+                  <a
+                    href={netflix_href}
+                    target="_blank"
+                    title={netflix_href}
+                    id="netflix_watchlink"
+                  >
+                    <img
+                      id="netflix"
+                      src={netflix_icon}
+                      width="100px"
+                      height="100px"
+                      opacity="1.0"
+                    />
+                  </a>
+                </div>
+                <div id="hulu_div">
+                  <a
+                    href={hulu_href}
+                    target="_blank"
+                    title={hulu_href}
+                    id="hulu_watchlink"
+                  >
+                    <img
+                      id="hulu"
+                      src={hulu_icon}
+                      width="100px"
+                      height="100px"
+                      opacity="1.0"
+                    />
+                  </a>
+                </div>
+                <div id="vudu_div">
+                  <a
+                    href={vudu_href}
+                    target="_blank"
+                    title={vudu_href}
+                    id="vudu_watchlink"
+                  >
+                    <img
+                      id="vudu"
+                      src={vudu_icon}
+                      width="100px"
+                      height="100px"
+                      opacity="1.0"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div id="itunes_fandango">
+                <div id="itunes_div">
+                  <a
+                    href={itunes_href}
+                    target="_blank"
+                    title={itunes_href}
+                    id="itunes_watchlink"
+                  >
+                    <img
+                      id="itunes"
+                      src={itunes_icon}
+                      width="100px"
+                      height="100px"
+                      opacity="1.0"
+                    />
+                  </a>
+                </div>
+                <div id="fandango_div">
+                  <a
+                    href={fandango_href}
+                    target="_blank"
+                    title={fandango_href}
+                    id="fandango_watchlink"
+                    opacity="1.0"
+                  >
+                    <img
+                      id="fandango"
+                      src="https://img04.mgo-images.com/image/static/content/web/logos/fandango-now.png"
+                      width="270px"
+                      height="90px"
+                    />
+                  </a>
+                </div>
+              </div>
             </div>
-            <div id="tomatometer" style={{ backgroundColor: "#abc" }}>
-              Tomatometer here
-            </div>
-            <div id="cast" style={{ backgroundColor: "#cdc" }}>
-              Cast details
+            <div id="plot">
+              PLOT:
+              <div id="plot_Val">{plotval}</div>
             </div>
           </div>
-          <div id="twitterfeed">
-            <div id="news">NEWS</div>
-            <div id="feed"> Twitter Feed</div>
-          </div>
-
-          <div id="screen_location">
-            <div id="location">LOCATION</div>
-            <div id="maps"> Maps</div>
-          </div>
+          <div id="row2"></div>
+          <div id="row3"></div>
         </div>
       </div>
     );

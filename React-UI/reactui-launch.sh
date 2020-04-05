4a6b457479d714abbf4d8936c08c0c172d783d23
srihaasapidikiti <srihaasa.pidikiti@colorado.edu>
2020-04-05 13:44:50 -0600

    launch scripts

--- React-UI/reactui-launch.sh
+++ React-UI/reactui-launch.sh
@@ -2,8 +2,10 @@
 #
 # This is the script you need to provide to launch a reactui service
 #
+npm run-script build
 docker build -t gcr.io/golden-hook-269722/reactui:v1 .
 docker push gcr.io/golden-hook-269722/reactui:v1
+kubectl apply -f deployment.yaml
 #docker run --rm --network network1 -p  7070:7070 gcr.io/green-entity-251200/rest:v1
-kubectl run reactui --image=gcr.io/golden-hook-269722/reactui:v1 
-kubectl expose deployment reactui --port 80 --target-port 80
+#kubectl run reactui --image=gcr.io/golden-hook-269722/reactui:v1 
+#kubectl expose deployment reactui --port 80 --target-port 80
